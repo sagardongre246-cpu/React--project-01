cfb6dbbddf90e0c0a4f3e0abe740dc76bc18d417
Sagar Dongare <sagardongre246@gmail.com>
2025-10-22 14:06:41 +0530

    first commit

---
+++ practice-two/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <p>Hello everyone Iam Sagar Dongare & I'm a Web developer at SkillCraft Technology</p>
+    </>
+  )
+}
+
+export default App
